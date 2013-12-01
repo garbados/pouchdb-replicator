7c60d8967a3fc596a1ee4d76e676287c9d294f54
Max Thayer <garbados@gmail.com>
2013-11-30 19:30:01 -0500

    testing via travis and coveralls

---
+++ test/fixtures.js
@@ -0,0 +1,12 @@
+exports.dbs = [
+  'db1',
+  'db2'
+];
+
+exports.docs = [
+  {
+    'omg': 1
+  }, {
+    'dom': 2
+  }
+];
--- test/index.js
+++ test/index.js
@@ -1,40 +1,46 @@
 var replicator = require('../'),
+    fixtures = require('./fixtures'),
     pouchdb = replicator(require('pouchdb')),
-    async = require('async');
+    async = require('async'),
+    assert = require('assert');
 
-var docs = [
-  {
-    'omg': 1
-  }, {
-    'dom': 2
-  }
-];
+describe('replicator', function () {
+  var dbs = fixtures.dbs,
+      docs = fixtures.docs;
 
-async.series([
-  function (done) {
-    var db1 = new pouchdb('db1');
+  beforeEach(function (done) {
+    var db1 = new pouchdb(dbs[0]);
     db1.bulkDocs({docs: docs}, done);
-  },
-  function (done) {
-    pouchdb.replicator().post({
-      source: 'db1',
-      target: 'db2',
-      create_target: true,
-      complete: done
-    });
-  }
-], function (err) {
-  if (err) {
-    throw err;
-  } else {
-    var results = new pouchdb('db2');
+  });
 
-    results.allDocs(function (err, res) {
-      if (err) {
-        throw err;
-      } else if (res.rows.length !== docs.length) {
-        throw [res.rows.length, '!==', docs.length].join(' ');
+  afterEach(function (done) {
+    var dbs_to_destroy = dbs
+          .concat('_replicator')
+          .map(function (db_name) {
+            return pouchdb.destroy.bind(pouchdb, db_name);
+          });
+
+    async.parallel(dbs_to_destroy, done);
+  });
+
+  it('should replicate docs', function (done) {
+    async.series([
+      function (done) {
+        pouchdb.replicator().post({
+          source: dbs[0],
+          target: dbs[1],
+          create_target: true,
+          complete: done
+        });
+      },
+      function (done) {
+        var results = new pouchdb(dbs[1]);
+
+        results.allDocs(function (err, res) {
+          assert.equal(res.rows.length, docs.length);
+          done(err);
+        });
       }
-    });
-  }
+    ], done);
+  });
 });
